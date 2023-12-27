12d4df6a45188d02d48090251f5eddeb2a1ed986
MOUAD-EL01 <elattarmouad651@gmail.com>
2023-12-27 19:42:14 +0100

    add to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -61,6 +61,7 @@ const Footer = () => {
         <div className="border bg-gray-20" />
         <p className="regular-14 w-full text-center text-gray-30">2023 Hilink | All rights reserved</p>
       </div>
+      <center>Made by Mouad Elattar</center>
     </footer>
   )
 }
